0ea6031198e2ca2147ea0dc687f469d9f1b6a9e6
salsabillaoctav <90443648+salsabillaoctav@users.noreply.github.com>
2021-09-23 11:28:02 +0700

    Add files via upload

---
+++ Salsabilla Octavianingrum_21091397005_Warnet.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    int sisa, Total_detik_mulai, Total_detik_selesai, jam_mulai, menit_mulai, detik_mulai, jam_selesai, menit_selesai, detik_selesai, lama_pemakaian, menit_pemakaian, detik_pemakaian, durasi, tot_bayar;
+
+
+printf("MULAI\n");
+    printf("JAM MULAI  =");
+    scanf("%i",&jam_mulai);
+    printf("MENIT MULAI  =");
+    scanf("%i",&menit_mulai);
+    printf("DETIK MULAI  =");
+    scanf("%i",&detik_mulai);
+    Total_detik_mulai= (jam_mulai*3600)+ (menit_mulai*60)+ detik_mulai;
+    printf("\n \n");
+
+printf("SELESAI\n");
+    printf("JAM SELESAI  =");
+    scanf("%i",&jam_selesai);
+    printf("MENIT SELESAI  =");
+    scanf("%i",&menit_selesai);
+    printf("DETIK SELESAI  =");
+    scanf("%i",&detik_selesai);
+    Total_detik_selesai= (jam_selesai*3600)+ (menit_selesai*60)+ detik_selesai;
+    durasi= Total_detik_selesai-Total_detik_mulai;
+
+tot_bayar= 1.388889* durasi;
+    lama_pemakaian= durasi/3600;
+    sisa=  durasi%3600;
+    menit_pemakaian= sisa/60;
+    detik_pemakaian= sisa%60;
+    printf("\n \n");
+
+printf("LAMA PEMAKAIAN\n");
+    printf("::: %i jam\n", lama_pemakaian);
+    printf("::: %i menit\n", menit_pemakaian);
+    printf("::: %i detik\n", detik_pemakaian);
+    printf("\n \n");
+printf("Tarif = Rp. 5.000,- per jam\n");
+    printf("TOTAL BIAYA YANG HARUS DIBAYAR: Rp. %i", tot_bayar);
+    printf("\n");
+    getch();
+}
